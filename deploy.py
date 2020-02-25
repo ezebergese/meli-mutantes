4001f583b1f14cd3fa2deffef82c6eba015229b2
Chris <fernandezs.christian@gmail.com>
2020-02-25 00:16:42 -0300

    passing credentials to aws cli

--- deploy.py
+++ deploy.py
@@ -1,4 +1,5 @@
 import subprocess
+import shlex
 import os.path
 import json
 
@@ -8,11 +9,14 @@ def deploy():
     role_session_name = 'DeployAwsSession'
     print("Assuming role...")
     command = 'aws sts assume-role --role-arn {} --role-session-name {}'
-    output = subprocess.getoutput(command.format(role_arn, role_session_name))
-    output_json = json.loads(output)
-    key = output_json['Credentials']['AccessKeyId']
-    secret = output_json['Credentials']['SecretAccessKey']
-    access_token = output_json['Credentials']['SessionToken']
+    command = command.format(role_arn, role_session_name)
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+    response, _ = process.communicate()
+    print(response.decode('utf-8').strip())
+    json_object = json.loads(response.decode('utf-8').strip())
+    key = json_object['Credentials']['AccessKeyId']
+    secret = json_object['Credentials']['SecretAccessKey']
+    access_token = json_object['Credentials']['SessionToken']
     my_env = os.environ.copy()
     my_env["AWS_ACCESS_KEY_ID"] = key
     my_env["AWS_SECRET_ACCESS_KEY"] = secret
@@ -21,13 +25,17 @@ def deploy():
     print("Deploying to ecs...")
     command = 'aws ecs update-service --cluster meli-cluster --service meli-service ' \
     + '--service meli-service --desired-count 1 --force-new-deployment --region us-east-1'
-    output = subprocess.getoutput(command)
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, env=my_env)
+    response, _ = process.communicate()
+    print(response)
 
     print("Deploying lambda...")
     command = 'aws lambda update-function-code --function-name  updateDnaAnalysisResultStats ' \
     + '--zip-file fileb://./meli-mutantes-lambda/build/libs/meli-mutantes-lambda.jar ' \
     + '--region us-east-1'
-    output = subprocess.getoutput(command)
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, env=my_env)
+    response, _ = process.communicate()
+    print(response)
 
     return my_env
 
